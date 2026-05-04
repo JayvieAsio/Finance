b2cbddcfe84498b6605e3533d718e3eef1fbf7fc
iamjayv <jeybieantoqueasio@gmail.com>
2026-05-04 13:15:23 +0800

    udated

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,20 @@
-
-from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Transaction
 from .serializers import TransactionSerializer
 
-class TransactionViewSet(viewsets.ModelViewSet):
-    queryset = Transaction.objects.all().order_by('-date')
-    serializer_class = TransactionSerializer
+@api_view(['GET', 'POST'])
+def transactions(request):
+    if request.method == 'GET':
+        items = Transaction.objects.all()
+        serializer = TransactionSerializer(items, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TransactionSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
