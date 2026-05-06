b27f810cbba94e7365e3481322cc25045e0bd886
iamjayv <jeybieantoqueasio@gmail.com>
2026-05-06 19:55:56 +0800

    update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import transactions
+from .views import transactions, delete_transaction
 
 urlpatterns = [
     path('transactions/', transactions),
+    path('transactions/<int:pk>/', delete_transaction),
 ]
